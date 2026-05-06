8cf8d72b1a9a2fd80fa3cca57413c1013b5d871c
muriloalbuquerque <muriloalbuquerque@users.noreply.github.com>
2026-05-05 22:52:42 -0300

    feat: configura AuthModule no NestJS e cookie-parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.WEB_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const cookieParser = await import('cookie-parser');
+  app.use(cookieParser.default());
+
   await app.listen(process.env.API_PORT ?? 3333);
 }
 bootstrap();
