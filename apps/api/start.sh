cf06904dbf8dcb7808d7dcb12a5798d6475b0f3d
Joao Batista <joaobneto03@outlook.com>
2026-05-14 12:59:35 -0300

    fix: inline migration on startup, remove start.sh

--- apps/api/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-echo "Running migrations..."
-npx prisma migrate deploy --schema=/app/node_modules/.pnpm/@flor+database*/node_modules/@flor/database/prisma/schema.prisma
-echo "Starting API..."
-exec node dist/main.js
